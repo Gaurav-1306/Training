4643a48020b322111f990b8fd51816511ce634a3
Gaurav-Tiwary <gauravtiwari1306@gmail.com>
2023-03-11 15:38:10 +0530

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,3 @@
-// import React, { useState } from 'react';
-// import { graphql, useStaticQuery } from 'gatsby';
-// import YAMLData from '../data/project.yaml';
-// import '../styling/global.css';
-
-// const IndexPage = ({ data }) => {
-//   const projects = YAMLData;
-//   const [showVideosOnly, setShowVideosOnly] = useState(false);
-//   const [selectedStatus, setSelectedStatus] = useState('');
-
-//   const nodes = showVideosOnly
-//     ? projects.filter((project) => project.videos !== undefined && project.videos !== null && project.videos.trim() !== '')
-//     : projects.filter((project) => selectedStatus === '' || project.status === selectedStatus);
-
-//   const statusOptions = ['stable', 'alpha', 'beta'];
-
-//   return (
-  
-//     <div>
-//       <label>
-//         <input type="checkbox" checked={showVideosOnly} onChange={(event) => setShowVideosOnly(event.target.checked)} />
-//         Show modules with video links only
-//       </label>
-//       <br />
-//       <label>
-//         Filter by status:{' '}
-//         <select value={selectedStatus} onChange={(event) => setSelectedStatus(event.target.value)}>
-//           <option value="">All</option>
-//           {statusOptions.map((status) => (
-//             <option key={status} value={status}>
-//               {status}
-//             </option>
-//           ))}
-//         </select>
-//       </label>
-//       <div className="module-container">
-//         {nodes.map((project) => (
-//           <div key={project.id} className="module">
-//             <h2>{project.name}</h2>
-//             <p>{project.description}</p>
-//             <p>Status: {project.status}</p>
-//             <p>Repository: <a href={project.repository}>{project.repository}</a></p>
-//             <p>Webpage: <a href={project.webpage}>{project.webpage}</a></p>
-//             {project.videos && <p>Video: <a href={project.videos}>{project.videos}</a></p>}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default IndexPage;
 import React, { useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import classNames from 'classnames';
@@ -68,7 +16,7 @@ const IndexPage = ({ data }) => {
   const handleStatusChange = (event) => {
     setSelectedStatus(event.target.value);
   };
-
+  console.log(projects)
   return (
     <div>
       <label>
@@ -90,6 +38,7 @@ const IndexPage = ({ data }) => {
             ["betaStatus"]: project.status === 'beta',
           })}>
             <h2>{project.name}</h2>
+            <img src='https://leverageedublog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2019/09/23165047/Importance-of-Books.jpg' height={"100px"} width={'auto'} />
             <p>{project.description}</p>
             <p>Status: {project.status}</p>
             <p>Repository: <a href={project.repository}>{project.repository}</a></p>
